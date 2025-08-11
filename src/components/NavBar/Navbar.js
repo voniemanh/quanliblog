5bbb2a2aa6136f24e2bb33f59c111c17ac443ade
Anonymous Dev <anon@example.com>
2025-08-11 09:27:38 +0700

    sửa UI login

--- src/components/NavBar/Navbar.js
+++ src/components/NavBar/Navbar.js
@@ -1,15 +1,34 @@
 import "./NavBar.css";
 import '../../App.css';
+import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 
-function NavBar() {
+
+function NavBar({ currentUser, setCurrentUser }) {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    localStorage.removeItem('currentUser');
+    setCurrentUser(null);
+    navigate('/login');
+  };
   return (
     <nav className="navbar">
       <div className="left">
         <a href="/">Home</a>
       </div>
       <div className="right">
-        <a href="/login">Login</a>
-        <a href="/register">Register</a>
+        {currentUser ? (
+          <>
+            <span>Hello, {currentUser.nickname} | </span>
+            <span style={{ cursor: 'pointer', color: 'grey' }} onClick={handleLogout}>Logout</span>
+          </>
+        ) : (
+          <div>
+            <Link to="/login">Login</Link>
+            <Link to="/register">Register</Link>
+          </div>
+        )}
       </div>
     </nav>
   );
